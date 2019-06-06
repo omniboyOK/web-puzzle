d64e174bf07cb573c6fef91e3f4aafdb6eb33b7a
Omniboy <kachorritou@gmail.com>
2019-06-06 13:52:25 -0300

    Checkeo de condición de victoria funcionando

--- js/juego.js
+++ js/juego.js
@@ -14,6 +14,12 @@ var grilla = [
     [7, 8, 9]
 ];
 
+var grillaGanadora = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
 /* Estas dos variables son para guardar la posición de la pieza vacía. 
 Esta posición comienza siendo la [2, 2]*/
 var filaVacia = 2;
@@ -36,18 +42,34 @@ function mostrarInstrucciones(intrucciones) {
 y utilice actualizarUltimoMovimiento para mostrarlo en pantalla */
 function guardarUltimoMovimiento(direccion) {
   this.direccion = direccion;
+  movimientos.push(direccion);
   actualizarUltimoMovimiento(direccion);
 }
 
 /* Esta función va a chequear si el Rompecabezas esta en la posicion ganadora. 
 Existen diferentes formas de hacer este chequeo a partir de la grilla. */
 function chequearSiGano() {
-    //COMPLETAR
+    var posicionCorrecta = 0;
+    for(var i = 0; i < grilla.length; i++){
+      for(var j = 0; j < grilla[i].length; j++){
+        if(grilla[i][j] == grillaGanadora[i][j]){
+          posicionCorrecta++;
+          console.log("posicion correcta");
+        }
+      }
+    }
+
+    if(posicionCorrecta > 8){
+      return true;
+    } else {
+      console.log("posicion incorrecta");
+      return false;
+    }
 }
 
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
-    //COMPLETAR
+  alert("ganaste");
 }
 
 /* Función que intercambia dos posiciones en la grilla.
@@ -61,18 +83,25 @@ En vez de intercambiar esos valores vamos a terminar teniendo en ambas posicione
 Se te ocurre cómo solucionar esto con una variable temporal?
 */
 function intercambiarPosicionesGrilla(filaPos1, columnaPos1, filaPos2, columnaPos2) {
-    //COMPLETAR
+  var tempArr = grilla[filaPos2][columnaPos2];
+  grilla[filaPos2][columnaPos2] = grilla[filaPos1][columnaPos1];
+  grilla[filaPos1][columnaPos1] = tempArr;
 }
 
 // Actualiza la posición de la pieza vacía
 function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
-    //COMPLETAR
+    filaVacia = nuevaFila;
+    columnaVacia = nuevaColumna;
 }
 
 
 // Para chequear si la posicón está dentro de la grilla.
 function posicionValida(fila, columna) {
-    //COMPLETAR
+  this.fila = fila;
+  this.columna = columna;
+  if(columna >= 0 && columna <= 2 && fila >= 0 && fila <= 2){
+    return true;
+  } else return false;
 }
 
 /* Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando su posición con otro elemento.
@@ -95,12 +124,14 @@ function moverEnDireccion(direccion) {
     
   // Mueve pieza hacia la derecha, reemplazandola con la blanca
   else if (direccion === codigosDireccion.DERECHA) {
-    //COMPLETAR
+    nuevaColumnaPiezaVacia = columnaVacia - 1;
+    nuevaFilaPiezaVacia = filaVacia;
   }
     
   // Mueve pieza hacia la izquierda, reemplazandola con la blanca
   else if (direccion === codigosDireccion.IZQUIERDA) {
-    // COMPLETAR
+    nuevaColumnaPiezaVacia = columnaVacia + 1;
+    nuevaFilaPiezaVacia = filaVacia;
   }
 
   /* A continuación se chequea si la nueva posición es válida, si lo es, se intercambia. 
@@ -114,7 +145,7 @@ function moverEnDireccion(direccion) {
   //COMPLETAR: Agregar la dirección del movimiento al arreglo de movimientos
   guardarUltimoMovimiento(direccion);
 
-    }
+    } else console.log('posicion no valida');
 }
 
 //////////////////////////////////////////////////////////
@@ -241,7 +272,7 @@ function capturarTeclas() {
               }, 500);
             }
             evento.preventDefault();
-        }
+        } else capturarTeclas();
     })
 }
 
